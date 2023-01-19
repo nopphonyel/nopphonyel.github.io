4feaf4935d2adfca18b7928956fb6ec1281dd64a
Markulox <nopphonyel@gmail.com>
2023-01-20 03:05:52 +0700

    remove unnecessary variables.

--- tools/ovonameenc.js
+++ tools/ovonameenc.js
@@ -4,19 +4,19 @@ btn_enc = document.getElementById("btn_enc");
 p_enc_name = document.getElementById("enc_name");
 
 function run() {
-    var name = inp_name.value;
-    const arr = new Int8Array(name.length);
-    var res = "";
+    let name = inp_name.value;
+    let res = "";
 
     console.log(name.length)
 
-    for(let i = 0; i < name.length; i++){
-        var n = name.charCodeAt(i);
-        var code_back = n & 15;
-        var code_fron = (n & 240) >> 4;
+    let n, code_back, code_fron;
+    for (let i = 0; i < name.length; i++) {
+        n = name.charCodeAt(i);
+        code_back = n & 15;
+        code_fron = (n & 240) >> 4;
         res += code_fron.toString(16) + code_back.toString(16)
     }
     p_enc_name.innerText = res
 
-};
+}
 
